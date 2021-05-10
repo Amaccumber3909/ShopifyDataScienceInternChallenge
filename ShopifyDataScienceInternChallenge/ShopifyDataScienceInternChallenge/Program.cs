8736a1a9ae461194d668988d8d699d5b5e9bb559
Alex MacCumber <Alexander.MacCumber@outlook.com>
2021-05-10 00:07:21 -0400

    Average Calculation Implemented
    Program calculates and outputs AOV to console

--- ShopifyDataScienceInternChallenge/ShopifyDataScienceInternChallenge/Program.cs
+++ ShopifyDataScienceInternChallenge/ShopifyDataScienceInternChallenge/Program.cs
@@ -32,13 +32,25 @@ namespace ShopifyDataScienceInternChallenge
         {
 
             char delimiter = ',';
+            int firstDataRow = 1;
+            int indexOrderAmount = 3;
+            int indexTotalItems = 4;
+            int totalItemsOutlierAmount = 10;
 
             // variables to hold the data
             List<string[]> dataFields = null;
 
+            // Variables to hold the two sum values
+            float orderAmountWithOutliers = 0;
+            float orderAmountNoOutliers = 0;
 
+            // Used to calculate the average after getting the sums
+            int totalItemsWithOutliers = 0;
+            int totalItemsNoOutliers = 0;
+
+            // Contains the averages
             float aovWithOutliers = 0;
-            float aovWithoutOutliers = 0;
+            float aovNoOutliers = 0;
 
             // Get the data from the file and process it to calculate the AOVs
             string[] data = null;
@@ -49,39 +61,54 @@ namespace ShopifyDataScienceInternChallenge
                 // Get the data from the csv file passed in the console arguments
                 while ((currentRow = reader.ReadLine()) != null)
                 {
-                    Console.WriteLine(currentRow);
                     data = currentRow.Split(delimiter);
                     i++;
+
+                    // Now we will add the values to the various variables
+                    
+                    // Check to see if we are past the header row
+                    if (i >  firstDataRow)
+                    {
+                        // Check to see if the total_items is larger than the outlier cap
+                        if (int.Parse(data[indexTotalItems]) < totalItemsOutlierAmount)
+                        {
+                            // total_items amount is not considered an outlier order
+                            orderAmountNoOutliers += int.Parse(data[indexTotalItems]);
+                            orderAmountNoOutliers += int.Parse(data[indexOrderAmount]);
+                            totalItemsNoOutliers++;
+                        }
+
+                        // Add the values for the outlier calculations
+                        orderAmountWithOutliers += int.Parse(data[indexTotalItems]);
+                        orderAmountWithOutliers += int.Parse(data[indexOrderAmount]);
+                        totalItemsWithOutliers++;
+                    }
                 }
             }
 
             // Now calculate the AOVs
 
-            // aovWithOutliers = AverageWithOutliers(dataFields);
-            // aovWithoutOutliers = AverageNoOutliers(dataFields);
 
+            Console.WriteLine("AOV with Outlier Data");
+            aovWithOutliers = CalculateAverage(orderAmountWithOutliers, totalItemsWithOutliers);
+            Console.WriteLine("AOV: $" + aovWithOutliers.ToString("0.00"));
+
+            Console.WriteLine();
+
+            Console.WriteLine("AOV without Outlier Data (10 Items or Less)");
+            aovNoOutliers = CalculateAverage(orderAmountNoOutliers, totalItemsNoOutliers);
+            Console.WriteLine("AOV: $" + aovNoOutliers.ToString("0.00"));
 
 
         }
 
 
-        static private float AverageNoOutliers(string[] data)
+        // Calculate the average given the sum of the order_amounts and the sum of the total_items data fields
+        static private float CalculateAverage(float orderAmountSum, int totalItems)
         {
             float avg = 0;
 
-
-
-
-            return avg;
-        }
-
-
-        static private float AverageWithOutliers(string[] data)
-        {
-            float avg = 0;
-
-
-
+            avg = orderAmountSum / totalItems;
 
             return avg;
         }
